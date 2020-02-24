9401131cab7a05f43259f7fe49239c050de19217
kishima <kishima7@gmail.com>
2020-02-25 08:01:16 +0900

    update sample

--- src/binary_app/01_run_string/testapp_src.c
+++ src/binary_app/01_run_string/testapp_src.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
 #include "mruby.h"
 #include "mruby/compile.h"
-#include "mruby/string.h"
-
-const char* script = "puts 'Hello mruby!'";
 
 int main(){
   mrb_state *mrb = mrb_open();
-  int ai = mrb_gc_arena_save(mrb);
-
-  mrb_load_string(mrb,script);
-  if (mrb->exc) {
-    printf(">Exception occurred: %s\n", mrb_str_to_cstr(mrb, mrb_inspect(mrb, mrb_obj_value(mrb->exc))));
-    mrb->exc = 0;
-  } else {
-    printf(">Success\n");
-  }
-  mrb_gc_arena_restore(mrb, ai);
+  mrb_load_string(mrb,"puts 'Hello mruby!'");
   mrb_close(mrb);
   return 0;
 }
